0e94e21eb163c1c55f893f90433a9efa675932ba
Heykel Hernández <heykelh33@gmail.com>
2017-10-30 12:38:07 -0400

    Update routing.py

--- djangoAngular/djangoAngular/routing.py
+++ djangoAngular/djangoAngular/routing.py
@@ -6,15 +6,21 @@ from radix.consumers import ws_connect, ws_message, ws_disconnect
 # including optional matching on message attributes. In this example, we match
 # on a path prefix, and then include routing from the chat module.
 channel_routing = [
-    # Include sub-routing from an app.
-    include("radix.routing.websocket_routing", path=r"^/radix"),
-    include("sensors.routing.websocket_routing", path=r"^/sensorsradix"),
+    route('websocket.connect', ws_connect),
+    route('websocket.receive', ws_message),
+    route('websocket.disconnect', ws_disconnect),
+
+    
+    
+    
+    #include("radix.routing.websocket_routing", path=r"^/radix"),
+    #include("sensors.routing.websocket_routing", path=r"^/sensorsradix"),
 
 
     # Custom handler for message sending (see Room.send_message).
     # Can't go in the include above as it's not got a `path` attribute to match on.
-    include("radix.routing.custom_routing"),
-    include("sensors.routing.custom_routing"),
+    #include("radix.routing.custom_routing"),
+    #include("sensors.routing.custom_routing"),
 
     
 
